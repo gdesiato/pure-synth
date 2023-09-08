041e314e04cf841915b35c8dfa52bd67f8b2bd58
Giuseppe Desiato <giodesiato@gmail.com>
2023-09-08 14:57:06 -0400

    commit

---
+++ src/main/java/com/desiato/tuningsystems/TuningSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.desiato.tuningsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuningSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuningSystemsApplication.class, args);
+	}
+
+}

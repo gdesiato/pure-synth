2b67abdee7e0582f5f5c81b05c1094aed8974f32
Giuseppe Desiato <giodesiato@gmail.com>
2023-10-03 07:51:03 -0400

    logger

--- src/main/java/com/desiato/puresynth/services/PdService.java
+++ src/main/java/com/desiato/puresynth/services/PdService.java
@@ -1,7 +1,11 @@
 package com.desiato.puresynth.services;
 
 import com.desiato.puresynth.models.PdReceiverAdapter;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.puredata.core.PdBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -9,8 +13,11 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.IOException;
 
+
 @Service
 public class PdService {
+
+    private static final Logger logger = LoggerFactory.getLogger(PdService.class);
 
     @Autowired
     private PdReceiverAdapter pdReceiverAdapter;
@@ -18,10 +25,12 @@ public class PdService {
     private int patchHandle;
     private String patchPath;
 
-
     public PdService(@Value("${puredata.patch.path}") String patchPath) {
         this.patchPath = patchPath;
+    }
 
+    @PostConstruct
+    public void init() {
         try {
             // Initialize libpd.
             PdBase.openAudio(1, 2, 44100);
@@ -33,9 +42,14 @@ public class PdService {
             File patchFile = new File(patchPath);
             patchHandle = PdBase.openPatch(patchFile.getAbsolutePath());
         } catch (IOException e) {
-            // Handle exceptions.
-            e.printStackTrace();
+            // Log the exception.
+            logger.error("Error during PdService initialization", e);
         }
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        closePatch();
     }
 
     public void startAudio() {

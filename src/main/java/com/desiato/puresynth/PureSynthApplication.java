3ffe784cc1cfa0c62b229f79727749bd689afc43
Giuseppe Desiato <giodesiato@gmail.com>
2023-10-19 18:57:52 -0400

    project modification

--- src/main/java/com/desiato/puresynth/PureSynthApplication.java
+++ src/main/java/com/desiato/puresynth/PureSynthApplication.java
@@ -7,27 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PureSynthApplication {
-	private static final Logger logger = LoggerFactory.getLogger(PureSynthApplication.class);
-
-
-	static {
-		logger.info("Attempting to load libpdnative using loadLibrary");
-		try {
-			// Where does Java look for the library?
-			logger.info("java.library.path: " + System.getProperty("java.library.path"));
-
-			System.loadLibrary("pdnative");
-			logger.info("Loaded libpdnative using loadLibrary");
-		} catch (UnsatisfiedLinkError e) {
-			logger.error("Failed to load libpdnative using loadLibrary");
-		}
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(PureSynthApplication.class, args);
-
-		System.out.println("java.library.path: " + System.getProperty("java.library.path"));
-
-
 	}
 }

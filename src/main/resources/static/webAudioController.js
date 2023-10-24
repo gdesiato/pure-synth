4d984a85503f5e94116f5d617a8b2c70d45d982f
Giuseppe Desiato <giodesiato@gmail.com>
2023-10-23 20:54:38 -0400

    waveforms

--- src/main/resources/static/webAudioController.js
+++ src/main/resources/static/webAudioController.js
@@ -1,18 +1,26 @@
 let context;
 let oscillator;
+let currentWaveform = 'sine'; // default waveform
 
 document.getElementById('playTone').addEventListener('click', function() {
-    playTone(440);
+    playTone(440, currentWaveform);
 });
 
 document.getElementById('stopTone').addEventListener('click', stopTone);
+
+const waveformButtons = document.querySelectorAll('.waveformButton');
+waveformButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        setWaveformType(this.dataset.waveform);
+    });
+});
 
 function initAudioContext() {
     // Initialize Audio Context
     context = new (window.AudioContext || window.webkitAudioContext)();
 }
 
-function playTone(frequency) {
+function playTone(frequency, waveformType) {
     if (!context) {
         initAudioContext();
     }
@@ -23,7 +31,8 @@ function playTone(frequency) {
     }
 
     oscillator = context.createOscillator();
-    oscillator.type = 'sine'; // wave type
+    oscillator.type = waveformType;
+    console.log('Oscillator type set to:', oscillator.type);
     oscillator.frequency.setValueAtTime(frequency, context.currentTime);
     oscillator.connect(context.destination);
     oscillator.start();
@@ -35,3 +44,14 @@ function stopTone() {
         oscillator = null;
     }
 }
+
+function setWaveformType(waveformType) {
+    console.log('Setting waveform to:', waveformType);
+    currentWaveform = waveformType;
+    if (oscillator) {
+        stopTone(); // Stop the current oscillator
+        playTone(440, waveformType); // Start a new oscillator with the new waveform type
+    }
+}
+
+

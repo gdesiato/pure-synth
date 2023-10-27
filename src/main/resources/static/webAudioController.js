150703b91cdc7ff4dbb65f42dad45089270a72f0
Giuseppe Desiato <giodesiato@gmail.com>
2023-10-27 07:34:17 -0400

    analyzer

--- src/main/resources/static/webAudioController.js
+++ src/main/resources/static/webAudioController.js
@@ -5,6 +5,14 @@ let currentFrequency = 440; // default frequency
 let gainNode; // volume
 let panner;
 
+let analyser;
+let dataArray;
+let bufferLength;
+let canvas;
+let canvasCtx;
+
+
+
 document.getElementById('playTone').addEventListener('click', function() {
     playTone(440, currentWaveform);
 });
@@ -36,6 +44,11 @@ document.getElementById('resetPan').addEventListener('click', function() {
     setPan(null, true);
 });
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    initAudioContext();
+    draw();
+});
+
 
 
 function initAudioContext() {
@@ -46,7 +59,20 @@ function initAudioContext() {
     gainNode.connect(panner);
     panner.connect(context.destination);
     setVolume(0.1); // Set the initial volume to 0.1
+
+    analyser = context.createAnalyser();
+    analyser.fftSize = 2048; // You can adjust this value as needed
+    bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
+
+    gainNode.connect(analyser);
+    analyser.connect(context.destination);
+
+    // Initialize the canvas
+    canvas = document.getElementById('visualizer');
+    canvasCtx = canvas.getContext('2d');
 }
+
 
 function playTone(frequency, waveformType) {
     if (!context) {
@@ -114,6 +140,39 @@ function setPan(value, reset = false) {
     }
 
     panner.pan.setValueAtTime(value, context.currentTime);
+}
+
+function draw() {
+    requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+    canvasCtx.beginPath();
+
+    let sliceWidth = canvas.width * 1.0 / bufferLength;
+    let x = 0;
+
+    for(let i = 0; i < bufferLength; i++) {
+
+        let v = dataArray[i] / 128.0;
+        let y = v * canvas.height / 2;
+
+        if (i === 0) {
+            canvasCtx.moveTo(x, y);
+        } else {
+            canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.stroke();
 }
 
 

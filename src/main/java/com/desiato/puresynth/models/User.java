c9b0cb34e7784854f9ddf61a7e2e3e96c8cd347d
Giuseppe Desiato <giodesiato@gmail.com>
2023-11-23 09:06:31 -0500

    change user

--- src/main/java/com/desiato/puresynth/models/User.java
+++ src/main/java/com/desiato/puresynth/models/User.java
@@ -2,13 +2,16 @@ package com.desiato.puresynth.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Data
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +24,7 @@ public class User {
     private String email;
 
     @Column(nullable = false)
-    private String password;  // Remember to hash and salt passwords before storing!
+    private String password;  // hash and salt passwords before storing!
 
     @ElementCollection
     private Set<String> userPreferences;
@@ -33,4 +36,28 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }

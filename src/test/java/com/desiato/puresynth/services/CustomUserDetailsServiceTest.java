9041e3b0f0498ed87891499f8bcaf703ad1644a1
Giuseppe Desiato <giodesiato@gmail.com>
2023-11-26 09:15:08 -0500

    test

--- src/test/java/com/desiato/puresynth/services/CustomUserDetailsServiceTest.java
+++ src/test/java/com/desiato/puresynth/services/CustomUserDetailsServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;

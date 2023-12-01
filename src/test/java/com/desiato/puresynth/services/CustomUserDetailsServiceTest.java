6452b9f6e73303570b323d0ccf7c4b42f5214473
Giuseppe Desiato <giodesiato@gmail.com>
2023-12-01 13:29:10 -0500

    roleuser

--- src/test/java/com/desiato/puresynth/services/CustomUserDetailsServiceTest.java
+++ src/test/java/com/desiato/puresynth/services/CustomUserDetailsServiceTest.java
@@ -1,6 +1,8 @@
 package com.desiato.puresynth.services;
 
+import com.desiato.puresynth.models.Role;
 import com.desiato.puresynth.models.User;
+import com.desiato.puresynth.repositories.RoleRepository;
 import com.desiato.puresynth.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -23,6 +28,9 @@ public class CustomUserDetailsServiceTest {
     private UserRepository userRepository;
 
     @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @BeforeEach
@@ -34,6 +42,22 @@ public class CustomUserDetailsServiceTest {
             user.setUsername("testUser");
             user.setPassword(passwordEncoder.encode("testPassword"));
             user.setEmail("testUser@mail.com");
+
+            // Fetch or create the role
+            Role userRole = roleRepository.findByName("ROLE_USER"); // role name
+            if (userRole == null) {
+                // Create the role if it doesn't exist
+                userRole = new Role();
+                userRole.setName("ROLE_USER"); // role name
+                roleRepository.save(userRole);
+            }
+
+            // Set the role to the user
+            Set<Role> roles = new HashSet<>();
+            roles.add(userRole);
+            user.setRoles(roles);
+
+            // Save the user with the role
             userRepository.save(user);
         }
     }

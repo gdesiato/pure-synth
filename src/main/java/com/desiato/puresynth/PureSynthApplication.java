c799576ec8fbc687bc821f66f075051f0c9afa5a
Giuseppe Desiato <giodesiato@gmail.com>
2023-12-02 17:27:54 -0500

    default user

--- src/main/java/com/desiato/puresynth/PureSynthApplication.java
+++ src/main/java/com/desiato/puresynth/PureSynthApplication.java
@@ -1,12 +1,62 @@
 package com.desiato.puresynth;
 
+import com.desiato.puresynth.models.Role;
+import com.desiato.puresynth.models.User;
+import com.desiato.puresynth.repositories.RoleRepository;
+import com.desiato.puresynth.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
-public class PureSynthApplication {
-	public static void main(String[] args) {
+public class PureSynthApplication implements CommandLineRunner {
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	public static void main(String[] args) {
 		SpringApplication.run(PureSynthApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// Check if the default user already exists
+		if (userRepository.findByUsername("defaultUser") == null) {
+			// Create the default user
+			User defaultUser = new User();
+			defaultUser.setUsername("defaultUser");
+			defaultUser.setPassword(passwordEncoder.encode("defaultPassword"));
+			defaultUser.setEmail("defaultUser@mail.com");
+
+			// Fetch or create the default role
+			Role userRole = roleRepository.findByName("ROLE_USER");
+			if (userRole == null) {
+				userRole = new Role();
+				userRole.setName("ROLE_USER");
+				roleRepository.save(userRole);
+			}
+
+			// Assign the role to the user
+			Set<Role> roles = new HashSet<>();
+			roles.add(userRole);
+			defaultUser.setRoles(roles);
+
+			// Save the user
+			userRepository.save(defaultUser);
+		}
+	}
 }
+
+

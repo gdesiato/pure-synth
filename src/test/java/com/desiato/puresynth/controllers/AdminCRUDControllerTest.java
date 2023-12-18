832574cc6c16b9f2ada251b4bfc12c2c1e3d43c7
Giuseppe Desiato <giodesiato@gmail.com>
2023-12-17 20:11:24 -0500

    test getUsers

--- src/test/java/com/desiato/puresynth/controllers/AdminCRUDControllerTest.java
+++ src/test/java/com/desiato/puresynth/controllers/AdminCRUDControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
@@ -29,6 +30,7 @@ public class AdminCRUDControllerTest {
     private UserService userService;
 
     @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
     public void testGetAllUsers() throws Exception {
 
         when(userService.getAllUsers()).thenReturn(Collections.emptyList());
@@ -37,5 +39,4 @@ public class AdminCRUDControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
     }
-
 }

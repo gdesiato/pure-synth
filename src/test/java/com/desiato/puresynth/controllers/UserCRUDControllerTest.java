72c2d71325770e619436e2d9dcc23841fcc64777
Giuseppe Desiato <giodesiato@gmail.com>
2024-01-07 08:20:01 -0500

    user crud

--- src/test/java/com/desiato/puresynth/controllers/UserCRUDControllerTest.java
+++ src/test/java/com/desiato/puresynth/controllers/UserCRUDControllerTest.java
@@ -1,6 +1,8 @@
 package com.desiato.puresynth.controllers;
 
 import com.desiato.puresynth.models.User;
+import com.desiato.puresynth.repositories.RoleRepository;
+import com.desiato.puresynth.repositories.UserRepository;
 import com.desiato.puresynth.restControllers.UserCRUDController;
 import com.desiato.puresynth.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,6 +20,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
 
 
 @WebMvcTest(UserCRUDController.class)
@@ -27,6 +32,15 @@ public class UserCRUDControllerTest {
 
     @MockBean
     private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private RoleRepository roleRepository;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
 
 
     @Test
@@ -48,12 +62,12 @@ public class UserCRUDControllerTest {
     public void testRegisterUser() throws Exception {
         User newUser = new User();
         newUser.setUsername("newUser");
-        newUser.setEmail("newUser@example.com");
+        newUser.setEmail("newUser@test.com");
 
         User savedUser = new User();
         savedUser.setId(1L);
         savedUser.setUsername("newUser");
-        savedUser.setEmail("newUser@example.com");
+        savedUser.setEmail("newUser@test.com");
 
         when(userService.findByUsername("newUser")).thenReturn(null);
         when(userService.saveUser(any(User.class))).thenReturn(savedUser);
@@ -64,6 +78,6 @@ public class UserCRUDControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id", is(1)))
                 .andExpect(jsonPath("$.username", is("newUser")))
-                .andExpect(jsonPath("$.email", is("newUser@example.com")));
+                .andExpect(jsonPath("$.email", is("newUser@test.com")));
     }
 }

bfb7853516a51b97ed4247978a6718cba1d577cf
Giuseppe Desiato <giodesiato@gmail.com>
2024-02-09 11:16:05 -0500

    audio controller download file

--- src/main/java/com/desiato/puresynth/controllers/AudioController.java
+++ src/main/java/com/desiato/puresynth/controllers/AudioController.java
@@ -3,17 +3,27 @@ package com.desiato.puresynth.controllers;
 import com.desiato.puresynth.models.AudioRequest;
 import com.desiato.puresynth.services.AudioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import java.io.File;
 
 @RestController
 @RequestMapping("/audio")
 public class AudioController {
+
+    @Value("${audio.files.directory}")
+    private String audioFilesDir;
 
     @Autowired
     private AudioService audioService;
@@ -27,6 +37,24 @@ public class AudioController {
         } catch (Exception e) {
             // Exception handling
             return ResponseEntity.internalServerError().body("Error generating audio: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> getAudio(@PathVariable String filename) {
+        try {
+            Path file = Paths.get(audioFilesDir).resolve(filename).normalize();
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                        .body(resource);
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
         }
     }
 }

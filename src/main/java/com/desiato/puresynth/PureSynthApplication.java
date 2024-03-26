5333502a9c94fc24c0de000c64df8ee0fa9df4dd
Giuseppe Desiato <giodesiato@gmail.com>
2024-03-26 18:24:19 -0400

    conflict solved main class

--- src/main/java/com/desiato/puresynth/PureSynthApplication.java
+++ src/main/java/com/desiato/puresynth/PureSynthApplication.java
@@ -1,13 +1,26 @@
 package com.desiato.puresynth;
 
+import com.desiato.puresynth.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class PureSynthApplication {
+
+public class PureSynthApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PureSynthApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
 

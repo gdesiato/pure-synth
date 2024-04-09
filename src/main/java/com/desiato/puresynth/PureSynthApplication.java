cc1c6185e78ffb7d09f59666bacb6dd11e3e6846
Giuseppe Desiato <10553375+gdesiato@users.noreply.github.com>
2024-04-09 08:16:22 -0400

    removed CommandLineRunner, run() and UserRepository from PureSynthApplication.java

--- src/main/java/com/desiato/puresynth/PureSynthApplication.java
+++ src/main/java/com/desiato/puresynth/PureSynthApplication.java
@@ -1,27 +1,18 @@
 package com.desiato.puresynth;
 
-import com.desiato.puresynth.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 
-public class PureSynthApplication implements CommandLineRunner {
-
-	@Autowired
-	private UserRepository userRepository;
+public class PureSynthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PureSynthApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
 }
 
 

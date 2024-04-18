a04513e22c4dfad16a7b17dc5d5fc58fa349c8ec
Giuseppe Desiato <giodesiato@gmail.com>
2024-04-17 20:04:50 -0400

    UUID implemented

--- src/test/java/com/desiato/puresynth/models/UserControllerTest.java
+++ src/test/java/com/desiato/puresynth/models/UserControllerTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -22,25 +24,27 @@ public class UserControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+
     @Test
     public void createUser_ShouldReturnCreatedUser() throws Exception {
         String uniqueEmail = generateUniqueEmail();
-        String newUserJson = """
+        String newUserJson = String.format("""
     {
         "email": "%s",
         "password": "password123"
     }
-    """.formatted(uniqueEmail);
+    """, uniqueEmail);
 
         mockMvc.perform(post("/api/user")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(newUserJson))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.email").value(uniqueEmail));
+                .andExpect(jsonPath("$.email").value(uniqueEmail))
+                .andDo(print());
     }
 
     private String generateUniqueEmail() {
-        return "test_" + System.currentTimeMillis() + "@example.com";
+        return "user_" + UUID.randomUUID().toString() + "@example.com";
     }
 
 }

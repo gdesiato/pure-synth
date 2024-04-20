170d4f5f9d092925d4cc0235f70891535a5e3f39
Giuseppe Desiato <giodesiato@gmail.com>
2024-04-19 21:06:31 -0400

    delete user test

--- src/test/java/com/desiato/puresynth/models/UserControllerTest.java
+++ src/test/java/com/desiato/puresynth/models/UserControllerTest.java
@@ -1,12 +1,14 @@
 package com.desiato.puresynth.models;
 
 import com.desiato.puresynth.repositories.UserRepository;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.UUID;
 
@@ -41,6 +43,28 @@ public class UserControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.email").value(uniqueEmail))
                 .andDo(print());
+    }
+
+    @Test
+    public void deleteUser_ShouldDeleteUserAndReturnOk() throws Exception {
+        String uniqueEmail = generateUniqueEmail();
+        String newUserJson = String.format("""
+    {
+        "email": "%s",
+        "password": "password123"
+    }
+    """, uniqueEmail);
+
+        MvcResult result = mockMvc.perform(post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newUserJson))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        Long userId = JsonPath.parse(result.getResponse().getContentAsString()).read("$.id", Long.class);
+
+        mockMvc.perform(delete("/api/user/" + userId))
+                .andExpect(status().isOk());
     }
 
     private String generateUniqueEmail() {

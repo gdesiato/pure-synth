f412954045046a68bb8b6da3adc36076ab5431fb
Giuseppe Desiato <giodesiato@gmail.com>
2024-05-20 22:00:30 +0200

    BaseTest class

---
+++ src/test/java/com/desiato/puresynth/BaseTest.java
@@ -0,0 +1,35 @@
+package com.desiato.puresynth;
+
+import com.desiato.puresynth.repositories.UserRepository;
+import com.desiato.puresynth.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BaseTest {
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Autowired
+    public UserService userService;
+
+//    @Autowired
+//    public SessionRepository sessionRepository;
+
+    @Autowired
+    public UserRepository userRepository;
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+
+//    @Autowired
+//    public AuthenticationService authService;
+//
+//    @Autowired
+//    public AuthTokenFilter authTokenFilter;
+}

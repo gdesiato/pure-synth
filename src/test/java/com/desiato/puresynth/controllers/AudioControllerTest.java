4e48424b0e139a584e8f9edafba339784bf36a3d
Giuseppe Desiato <giodesiato@gmail.com>
2024-10-16 20:05:56 +0200

    updated audio geneartion test to check the size of the audio file

--- src/test/java/com/desiato/puresynth/controllers/AudioControllerTest.java
+++ src/test/java/com/desiato/puresynth/controllers/AudioControllerTest.java
@@ -5,6 +5,8 @@ import com.desiato.puresynth.dtos.AuthenticatedUser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,17 +28,27 @@ class AudioControllerTest extends BaseTest {
         String audioJson = """
                 {
                     "frequency" : 501,
-                    "duration" : 1000
+                    "duration" : 1
                 }
                 """;
 
-        mockMvc.perform(post("/api/audio/generate")
+        MvcResult result = mockMvc.perform(post("/api/audio/generate")
                         .header("Authorization", "Bearer " + authenticatedUser.pureSynthToken().value())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(audioJson))
-                .andExpect(status().isOk())
-                .andExpect(header().string("Content-Disposition",
-                        "attachment; filename=\"sine_wave_501.0Hz.wav\"")) // Check the header
-                .andExpect(content().contentType("audio/wav"));
+                .andExpect(status().isOk())  // Expecting 200 OK
+                .andExpect(header().string("Content-Disposition", "attachment; filename=\"sine_wave_501.0Hz.wav\""))
+                .andExpect(content().contentType("audio/wav"))
+                .andReturn();
+
+        // Get the response body as bytes
+        MockHttpServletResponse response = result.getResponse();
+        byte[] audioBytes = response.getContentAsByteArray();
+
+        // Calculate the size of the generated file in bytes
+        int fileSizeInBytes = audioBytes.length;
+
+        System.out.println("Generated audio file size: " + fileSizeInBytes + " bytes");
+
     }
 }
